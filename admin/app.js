210a8eb804dd4df8751684dbcf2973f4dc63cd0a
Mogo <checador.mogo@gmail.com>
2025-12-19 12:36:30 -0600

    update url2

--- admin/app.js
+++ admin/app.js
@@ -117,7 +117,7 @@ closeWorkersModal.onclick = () => {
 };
 
 function loadWorkers() {
-  fetch('https://script.google.com/macros/s/https://script.google.com/macros/s/AKfycbxm2zzRoqaE209KzJdBdfF9Urch4C76wAHm7wPPlUdS0J5Q-hOhMRoQgolQU4VXMEd8/exec')
+  fetch('https://script.google.com/macros/s/AKfycbxm2zzRoqaE209KzJdBdfF9Urch4C76wAHm7wPPlUdS0J5Q-hOhMRoQgolQU4VXMEd8/exec')
     .then(res => res.json())
     .then(data => {
       workersTableBody.innerHTML = '';
